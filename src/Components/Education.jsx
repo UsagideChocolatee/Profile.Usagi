1bfdefa400a22542ca56f13f692f5ecf3365f634
UsagideChocolate <154567644+UsagideChocolatee@users.noreply.github.com>
2024-01-04 03:45:42 +0000

    Añadida nueva sección education

---
+++ src/Components/Education.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="Dark" id="education" style={{ marginBottom: "2rem" }}>
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <p style={{ textAlign: "center" }}>
+        Microsoft Student Ambassador - Foreign Trade Student - Diploma in Environmental Project
+        Management.
+      </p>
+    </section>
+  );
+};
+
+export default Education;
